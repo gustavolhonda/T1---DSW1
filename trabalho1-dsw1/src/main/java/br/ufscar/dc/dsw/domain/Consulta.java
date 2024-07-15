255c2bda4cfb988dbcf8eac2c7a13d4ccb41bea8
Vitor Vechin <vitorvechin@gmail.com>
2024-07-15 17:02:23 -0300

    formata arquivos

--- trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/domain/Consulta.java
+++ trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/domain/Consulta.java
@@ -4,16 +4,16 @@ import java.util.Date;
 
 public class Consulta {
 
-	private String cpfCliente;
-	private String cpfProfissional;
-	private Date dataHora;
-	private Integer id;
+    private String cpfCliente;
+    private String cpfProfissional;
+    private Date dataHora;
+    private Integer id;
 
-	public Consulta(String cpfCliente, String cpfProfissional, Date dataHora, Integer id) {
-		this.cpfCliente = cpfCliente;
-		this.cpfProfissional = cpfProfissional;
-		this.dataHora = dataHora;
-	}
+    public Consulta(String cpfCliente, String cpfProfissional, Date dataHora, Integer id) {
+        this.cpfCliente = cpfCliente;
+        this.cpfProfissional = cpfProfissional;
+        this.dataHora = dataHora;
+    }
 
     public String getCpfCliente() {
         return cpfCliente;
@@ -31,23 +31,23 @@ public class Consulta {
         return id;
     }
 
-	public void setCpfCliente(String cpfCliente) {
-		this.cpfCliente = cpfCliente;
-	}
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
 
-	public void setCpfProfissional(String cpfProfissional) {
-		this.cpfProfissional = cpfProfissional;
-	}
+    public void setCpfProfissional(String cpfProfissional) {
+        this.cpfProfissional = cpfProfissional;
+    }
 
-	public void setDataHora(Date dataHora) {
-		this.dataHora = dataHora;
-	}
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public Consulta(Integer id) {
-		this.id = id;
-	}
+    public Consulta(Integer id) {
+        this.id = id;
+    }
 }
--- trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
+++ trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
@@ -2,81 +2,81 @@ package br.ufscar.dc.dsw.domain;
 
 public class Profissional {
 
-	private Integer id;
-	private String email;
-	private String senha;
-	private String cpf;
-  private String nome;
-  private String especialidade;
+    private Integer id;
+    private String email;
+    private String senha;
+    private String cpf;
+    private String nome;
+    private String especialidade;
 
-	public Profissional(Integer id) {
-		this.id = id;
-	}
+    public Profissional(Integer id) {
+        this.id = id;
+    }
 
-	public Profissional(String email, String senha, String cpf, String nome, String especialidade) {
-		super();
-		this.email = email;
-		this.senha = senha;
-		this.cpf = cpf;
-		this.nome = nome;
+    public Profissional(String email, String senha, String cpf, String nome, String especialidade) {
+        super();
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.nome = nome;
         this.especialidade = especialidade;
-	}
+    }
 
-	public Profissional(Integer id, String email, String senha, String cpf, String nome, String especialidade) {
-		super();
-		this.id = id;
-		this.email = email;
-		this.senha = senha;
-		this.cpf = cpf;
-		this.nome = nome;
-    this.especialidade = especialidade;
-	}
+    public Profissional(Integer id, String email, String senha, String cpf, String nome, String especialidade) {
+        super();
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.especialidade = especialidade;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public String getEspecialidade() {
-		return especialidade;
-	}
+    public String getEspecialidade() {
+        return especialidade;
+    }
 
-	public void setEspecialidade(String especialidade) {
-		this.especialidade = especialidade;
-	}
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
 
     public String getCpf() {
-		return cpf;
-	}
+        return cpf;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
