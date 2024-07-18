d57feb472d7614ccc74726957011626cade686be
Vitor Vechin <vitorvechin@gmail.com>
2024-07-18 14:32:38 -0300

    atualiza db

--- trabalho1-dsw1/db/MySQL/create.sql
+++ trabalho1-dsw1/db/MySQL/create.sql
@@ -1,60 +1,40 @@
-drop database if exists Consultas;
+DROP DATABASE IF EXISTS Consultas;
 
-create database Consultas;
+CREATE DATABASE Consultas;
 
-use Consultas;
+USE Consultas;
 
 CREATE TABLE Usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,
-    nome VARCHAR(255) NOT NULL,
-    telefone VARCHAR(15),
-    sexo CHAR(1),
-    dataNasc DATE,
-    especialidade VARCHAR(255),
-    tipo ENUM('Cliente', 'Profissional') NOT NULL
+    tipo ENUM('Cliente', 'Profissional', 'Administrador') NOT NULL
 );
 
-CREATE TABLE Clientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    nome VARCHAR(255) NOT NULL,
+CREATE TABLE Cliente (
+    id_usuario INT PRIMARY KEY,
     telefone VARCHAR(15) NOT NULL,
     sexo CHAR(1) NOT NULL,
-    dataNasc DATE NOT NULL
+    dataNasc DATE NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Profissionais (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    nome VARCHAR(255) NOT NULL,
+CREATE TABLE Profissional (
+    id_usuario INT PRIMARY KEY,
     especialidade VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Consultas (
+CREATE TABLE Agendamento (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente_cpf VARCHAR(11) NOT NULL,
-    profissional_cpf VARCHAR(11) NOT NULL,
+    id_cliente INT NOT NULL,
+    id_profissional INT NOT NULL,
     data_hora DATETIME NOT NULL,
     link_videoconferencia VARCHAR(255) NOT NULL,
-    FOREIGN KEY (cliente_cpf) REFERENCES Clientes(cpf),
-    FOREIGN KEY (profissional_cpf) REFERENCES Profissionais(cpf),
-    UNIQUE (profissional_cpf, data_hora),
-    UNIQUE (cliente_cpf, data_hora)
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_usuario) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_profissional) REFERENCES Profissional(id_usuario) ON DELETE CASCADE ON UPDATE CASCADE,
+    UNIQUE (id_profissional, data_hora),
+    UNIQUE (id_cliente, data_hora)
 );
-
-CREATE TABLE Admin (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL
-);
-
-
-
-
