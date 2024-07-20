e182de56d70fd430953030c292f084aea397cabd
Jmigliatti <140974522+Jmigliatti@users.noreply.github.com>
2024-07-20 20:10:45 -0300

    Update ProfissionalController.java

--- trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/controller/ProfissionalController.java
+++ trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/controller/ProfissionalController.java
@@ -1,7 +1,11 @@
 package br.ufscar.dc.dsw.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,137 +14,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.ufscar.dc.dsw.dao.AgendamentoDAO;
 import br.ufscar.dc.dsw.dao.ProfissionalDAO;
+import br.ufscar.dc.dsw.domain.Agendamento;
 import br.ufscar.dc.dsw.domain.Profissional;
 import br.ufscar.dc.dsw.util.Erro;
 
-@WebServlet(urlPatterns = "/profissionais/*")
+@WebServlet(urlPatterns = "/profissional/*")
 public class ProfissionalController extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
 
-	private ProfissionalDAO dao;
+		private AgendamentoDAO dao;
 
-	@Override
-	public void init() {
-		dao = new ProfissionalDAO();
-	}
+		@Override
+		public void init() {
+				dao = new AgendamentoDAO();
+		}
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		doGet(request, response);
-	}
+		@Override
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				doGet(request, response);
+		}
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		Profissional profissional = (Profissional) request.getSession().getAttribute("profissionalLogado");
+		Profissional profissional = (Profissional) request.getSession().getAttribute("usuarioLogado");
 		Erro erros = new Erro();
 
 		if (profissional == null) {
 			response.sendRedirect(request.getContextPath());
 			return;
-		} else if (!profissional.getPapel().equals("ADMIN")) {
+		} else if (!profissional.getPapel().equals("profissional")) {
 			erros.add("Acesso não autorizado!");
-			erros.add("Apenas Papel [ADMIN] tem acesso a essa página");
+			erros.add("Apenas Papel [profissional] tem acesso a essa página");
 			request.setAttribute("mensagens", erros);
 			RequestDispatcher rd = request.getRequestDispatcher("/noAuth.jsp");
 			rd.forward(request, response);
 			return;
 		}
-		
-		String action = request.getPathInfo();
-		if (action == null) {
-			action = "";
+
+				String action = request.getPathInfo();
+				if (action == null) {
+						action = "";
+				}
+
+				try {
+						switch (action) {
+								default:
+										lista(request, response);
+										break;
+						}
+				} catch (RuntimeException | IOException | ServletException e) {
+						throw new ServletException(e);
+				}
 		}
 
-		try {
-			switch (action) {
-			case "/cadastro":
-				apresentaFormCadastro(request, response);
-				break;
-			case "/insercao":
-				insere(request, response);
-				break;
-			case "/remocao":
-				remove(request, response);
-				break;
-			case "/edicao":
-				apresentaFormEdicao(request, response);
-				break;
-			case "/atualizacao":
-				atualize(request, response);
-				break;
-			default:
-				lista(request, response);
-				break;
-			}
-		} catch (RuntimeException | IOException | ServletException e) {
-			throw new ServletException(e);
+		private void lista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			Profissional profissional = (Profissional) request.getSession().getAttribute("usuarioLogado");
+				List<Agendamento> listaAgendamentos = dao.getAll(profissional);
+				request.setAttribute("listaAgendamentos", listaAgendamentos);
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/agendamento/lista.jsp");
+				dispatcher.forward(request, response);
 		}
-	}
 
-	private void lista(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Profissional> listaProfissionais = dao.getAll();
-		request.setAttribute("listaProfissionais", listaProfissionais);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/profissional/lista.jsp");
-		dispatcher.forward(request, response);
-	}
+		private Map<Integer, Agendamento> getAgendamentos() {
+				Map<Integer, Agendamento> agendamento = new HashMap<>();
+				for (Agendamento agendamento: new AgendamentoDAO().getAll()) {
+						agendamentos.put(agendamento.getId(), agendamento);
+				}
+				return agendamentos;
+		}
 
-	private void apresentaFormCadastro(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/profissional/formulario.jsp");
-		dispatcher.forward(request, response);
-	}
 
-	private void apresentaFormEdicao(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		Long id = Long.parseLong(request.getParameter("id"));
-		Profissional profissional = dao.get(id);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/logado/profissional/formulario.jsp");
-		request.setAttribute("profissional", profissional);
-		dispatcher.forward(request, response);
-	}
-
-	private void insere(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-
-		String email = request.getParameter("email");
-		String senha = request.getParameter("senha");
-		String cpf = request.getParameter("cpf");
-		String nome = request.getParameter("nome");
-		String especialidade = request.getParameter("especialidade");
-		
-		Profissional profissional = new Profissional(nome, email, senha, cpf, nome, especialidade);
-
-		dao.insert(profissional);
-		response.sendRedirect("lista");
-	}
-
-	private void atualize(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		request.setCharacterEncoding("UTF-8");
-		Integer id = Integer.parseInt(request.getParameter("id"));
-		String email = request.getParameter("email");
-		String senha = request.getParameter("senha");
-		String cpf = request.getParameter("cpf");
-		String nome = request.getParameter("nome");
-		String especialidade = request.getParameter("especialidade");
-		
-		Profissional profissional = new Profissional(id, email, senha, cpf, nome, especialidade);
-
-		dao.update(profissional);
-		response.sendRedirect("lista");
-	}
-
-	private void remove(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Interger id = Interger.parseInt(request.getParameter("id"));
-
-		Profissional profissional = new Profissional(id);
-		dao.delete(profissional);
-		response.sendRedirect("lista");
-	}
 }
