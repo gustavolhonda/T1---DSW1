c62adb6cc095cf187c5832d07c10525d3020e642
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2024-07-21 15:23:54 -0300

    chama o profissionalDAO

--- trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/controller/ListaController.java
+++ trabalho1-dsw1/src/main/java/br/ufscar/dc/dsw/controller/ListaController.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.ufscar.dc.dsw.dao.ProfissionalDAO;
 import br.ufscar.dc.dsw.dao.UsuarioDAO;
 import br.ufscar.dc.dsw.domain.Usuario;
+import br.ufscar.dc.dsw.domain.Profissional;
 import br.ufscar.dc.dsw.util.Erro;
 
 @WebServlet(name = "Lista", urlPatterns = { "/lista.jsp" })
@@ -24,9 +26,9 @@ public class ListaController extends HttpServlet {
 		Erro erros = new Erro();
 		if (request.getParameter("listar") != null) {
 			if (!erros.isExisteErros()) {
-				UsuarioDAO dao = new UsuarioDAO();
-				List<Usuario> listaProfissionais = dao.getProfissional();
-			request.setAttribute("listaProfissionais", listaProfissionais);
+				ProfissionalDAO dao = new ProfissionalDAO();
+				List<Profissional> listaProfissionais = dao.getAll();
+			  request.setAttribute("listaProfissionais", listaProfissionais);
      //temos que mudar essa URL 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/lista.jsp");
 			dispatcher.forward(request, response);
