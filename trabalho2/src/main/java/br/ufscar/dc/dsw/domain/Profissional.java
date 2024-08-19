858d9509bd7f1c36319a44102d38424ffe24f20a
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2024-08-19 00:28:23 -0300

    estruturando a classe profissional

---
+++ trabalho2/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
@@ -0,0 +1,43 @@
+package br.ufscar.dc.dsw.domain;
+
+import java.util.List;
+
+import br.ufscar.dc.dsw.validation.UniqueCNPJ;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "Profissional")
+@PrimaryKeyJoinColumn(name = "id_profissional") 
+public class Profissional extends Usuario {
+	
+	@NotBlank(message = "{NotBlank.profissional.especialidade}")
+	@Column(length = 100)
+	private String especialidade;
+
+	@Column(columnDefinition = "TEXT")
+	private String curriculo;
+
+	public String getEspecialidade() {
+			return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+			this.especialidade = especialidade;
+	}
+
+	public String getCurriculo() {
+			return curriculo;
+	}
+
+	public void setCurriculo(String curriculo) {
+			this.curriculo = curriculo;
+	}
+}
