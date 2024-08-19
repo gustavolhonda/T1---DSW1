126b00ae16e264c4ba70659924e4670571a7d684
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2024-08-18 22:32:48 -0300

    começando usuario

--- trabalho2/src/main/java/br/ufscar/dc/dsw/domain/Usuario.java
+++ trabalho2/src/main/java/br/ufscar/dc/dsw/domain/Usuario.java
@@ -23,7 +23,7 @@ public class Usuario extends AbstractEntity<Long> {
     private String name;
     
     @NotBlank
-    @Column(nullable = false, length = 14)
+    @Column(nullable = false, length = 14, unique = true)
     private String CPF;
     
     @NotBlank
