65d79ffe71996ee16904e75a6d23c6850c65626d
Vitor Vechin <vitorvechin@gmail.com>
2024-08-19 16:40:10 -0300

    arrumei comparação de strings

--- trabalho2/src/main/java/br/ufscar/dc/dsw/controller/AgendamentoController.java
+++ trabalho2/src/main/java/br/ufscar/dc/dsw/controller/AgendamentoController.java
@@ -2,23 +2,21 @@ package br.ufscar.dc.dsw.controller;
 
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.ufscar.dc.dsw.domain.Agendamento;
-import br.ufscar.dc.dsw.security.UsuarioDetails;
-import br.ufscar.dc.dsw.domain.Usuario;
 import br.ufscar.dc.dsw.domain.Cliente;
 import br.ufscar.dc.dsw.domain.Profissional;
+import br.ufscar.dc.dsw.domain.Usuario;
+import br.ufscar.dc.dsw.security.UsuarioDetails;
 import br.ufscar.dc.dsw.service.spec.IAgendamentoService;
 import br.ufscar.dc.dsw.service.spec.IClienteService;
 import br.ufscar.dc.dsw.service.spec.IProfissionalService;
@@ -46,7 +44,7 @@ public class AgendamentoController {
 	public String listar(ModelMap model) {
     Usuario user = getUsuario();
 
-		if (user.getRole() == "ROLE_CLIENT") {
+		if ("ROLE_CLIENT".equals(user.getRole())) {
             Cliente cliente = clienteService.buscarPorId(user.getId());
 			model.addAttribute("agendamento", cliente.getAgendamentos());
 		} else {
