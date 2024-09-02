9ed87aa2e6234c56579f0839b5f46fd2bb3d8249
Vitor Vechin <vitorvechin@gmail.com>
2024-09-02 14:42:42 -0300

    Altera WebSecurityConfig

--- T2/src/main/java/br/ufscar/dc/dsw/controller/ClienteRestController.java
+++ T2/src/main/java/br/ufscar/dc/dsw/controller/ClienteRestController.java
@@ -1,5 +1,30 @@
 package br.ufscar.dc.dsw.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufscar.dc.dsw.domain.Cliente;
+import br.ufscar.dc.dsw.service.spec.IClienteService;
+
+@RestController
 public class ClienteRestController {
+
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping(path = "/api/clientes")
+    public ResponseEntity<List<Cliente>> listaClientes() {
+        List<Cliente> lista = clienteService.buscarTodos();
+
+        if (lista.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(lista);
+    }
     
 }
